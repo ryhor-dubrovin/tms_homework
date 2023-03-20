c4811fbebf82eae1c6568ade21bfe766589b07bf
Ryhor <ryhordubrovin@gmail.com>
2023-03-20 21:46:19 +0400

    add SpaceX

--- HomeWork_8/src/Main.java
+++ HomeWork_8/src/Main.java
@@ -1,11 +1,15 @@
 import firstPart.IStart;
 import firstPart.Shuttle;
+import firstPart.SpaceX;
 import firstPart.Spaceport;
 
 public class Main {
     public static void main(String[] args) {
         Spaceport spaceport = new Spaceport();
         Shuttle shuttle = new Shuttle();
+        SpaceX teslaFly = new SpaceX();
+
         spaceport.launch(shuttle);
+        spaceport.launch(teslaFly);
     }
 }
