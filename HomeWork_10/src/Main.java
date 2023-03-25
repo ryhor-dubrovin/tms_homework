31714958f02d09761f401a32a8bd73ba4fee5dd2
Ryhor <ryhordubrovin@gmail.com>
2023-03-25 17:36:15 +0400

    first part of task

--- HomeWork_10/src/Main.java
+++ HomeWork_10/src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "I like Java !!";
+        System.out.println(str.charAt(str.length()-1));
+        System.out.println(str.endsWith("!!!"));
+        System.out.println(str.startsWith("I like"));
+        System.out.println(str.contains("Java"));
+        System.out.println(str.indexOf("Java"));
+        System.out.println(str.replace("a","o"));
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.replace(str.substring(7, 11), ""));
+        findPalindromesFromText(str);
+        find3to5SymbolsWords(str);
+
+    }
+    public static void findPalindromesFromText(String str) {
+        String[] strArr = new String[str.split(" ").length];
+        strArr = str.split(" ");
+
+        for (int i = 0; i < strArr.length; i++) {
+            strArr[i].length();
+        }
+    }
+
+    public static void find3to5SymbolsWords(String str) {
+        String[] strArr = new String[str.split(" ").length];
+        strArr = str.split(" ");
+        for (int i = 0; i < strArr.length; i++) {
+            if(strArr[i].length() >= 3 && strArr[i].length() <= 5) {
+                System.out.print(strArr[i] +"; ");
+            }
+        }
     }
 }
